c20cc4571c4a626a689e2387d09eede25b00c230
Alex9606 <144795176+Alex9606@users.noreply.github.com>
2023-09-12 14:37:25 +0300

    calculator

---
+++ calculator.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+
+   /* int a1 = INT32_MAX;
+    int a2 = INT32_MIN;
+    short a3 = SHRT_MAX;
+    short a4 = SHRT_MAX;
+    long long a5 = LONG_MAX;
+    long long a6 = LONG_MAX;
+    unsigned int a7 = 0;
+    unsigned int a8 = UINT32_MAX;
+    unsigned short a9 = USHRT_MAX;
+    unsigned long long a10 = ULONG_MAX;
+    float a11 = FLT_MAX;
+    float a12 = FLT_MIN;
+    double a13 = DBL_MAX;
+    double a14 = DBL_MIN;
+    char a15 = 0;
+    char a16 = 255;
+    bool a17 = 0;
+    bool a18 = 1;
+
+    cout << "type\t\t|byte\t|\tmin\t|max\n";
+    cout << "______________________________________________________\n";
+    cout << "int\t\t|"<<sizeof(a1)<<"\t|" << a1 << "\t|" << a2 << "\n";
+    cout << "short\t\t|" << sizeof(a6) << "\t|" << a6 << "\t|" << a5 << "\n";
+    cout << "long\t\t|" << sizeof(a5) << "\t|" << a5 << "\t|" << a6 << "\n";
+    cout << "unsigned int\t\t|" << sizeof(a7) << "\t|" << a7 << "\t|" << a8 << "\n";
+    cout << "unsigned short\t\t|" << sizeof(a9) << "\t|" << a9 << "\t|" << a10 << "\n";
+    cout << "unsigned long long\t\t|" << sizeof(a11) << "\t|" << a11 << "\t|" << a12 << "\n";
+    cout << "float\t\t|" << sizeof(a13) << "\t|" << a13 << "\t|" << a14 << "\n";
+    cout << "double\t\t|" << sizeof(a15) << "\t|" << a15 << "\t|" << a16 << "\n";
+    cout << "char\t\t|" << sizeof(a17) << "\t|" << a17 << "\t|" << a18 << "\n";
+    cout << "bool\t\t|" << sizeof(a1) << "\t|" << a1 << "\t|" << a2 << "\n";*/
+
+{
+
+    float a, b;
+    cout << "input a: ";
+    cin >> a ;
+    cout << "input b: ";
+    cin >> b ;
+    cout << a << "+" << b << "=" << a + b << "\n";
+    cout << a << "-" << b << "=" << a - b << "\n";
+    cout << a << "/" << b << " = " << a * b << "\n";
+    cout << a << "*" << b << "=" << a * b << "\n";
+}
+
